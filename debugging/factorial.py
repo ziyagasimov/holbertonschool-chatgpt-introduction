28b7c91a54ad1792d60758133069b16241788a88
ziyagasimov <ziyagasimov0@gmail.com>
2025-10-24 14:42:14 +0000

    t1

---
+++ debugging/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1   # Decrement n so the loop ends
+    return result
+
+if __name__ == "__main__":
+    f = factorial(int(sys.argv[1]))
+    print(f)
